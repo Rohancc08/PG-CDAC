9f8e1b15647d09775728acf2c6859b2c0306ffd2
demo-repo <133081690+Rohancc08@users.noreply.github.com>
2024-08-08 07:42:11 +0530

    Files Uploaded

---
+++ frontend/src/components/pages/About.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import NavBar from '../layout/Navbar'
+const About = () => {
+
+  return (
+    <div>
+    
+    <NavBar/>
+    <div className="container">
+      <div className="py-4 text-black blocktext" >
+        <h2>About Us</h2>
+        <br></br>
+        <p>
+          <strong>
+            Vehicle Service Management System aims to be the best of both worlds
+            - Reliable and Cost-effective Vehicle Services. <br />
+            Vehicle Maintenance and Repair is the web page of providing services related to vehicles like diagnostics, dents and paints, brakes repair, suspension repair, interior detailing changes and regular Maintenance. Owners of vehicles frequently inquire about service at auto shops. Vehicle technicians examine crucial parts of the vehicle and fix problems to make sure they are in good condition and won't break down suddenly.
+            We provide reliable, cost effective vehicle services that are tailored to meet your needs.
+          </strong>
+        </p>
+        <p>
+          Group Name:-  
+          <br />Title - Vehicle Servicing Management System
+        </p>
+        
+        <p>Member 1 -  </p>
+        <p>Member 2 -  </p>
+        <p>Member 1 -  </p>
+        <p>Member 2 -  </p>
+        
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/pages/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import NavBar from '../layout/Navbar'
+
+
+const Contact = () => {
+  return (
+    <div><NavBar/>
+    <div className="container" >
+      <div className="py-4" className="text-black blocktext">
+        <h3>Leave us a message!</h3>
+        
+        <br></br>
+        <h4>Name:- Datta N. Dandale</h4>
+        <h4>Helpline Numbers : +919850556135</h4>
+        <h4>Customer Support : +919850556135</h4>
+        <h4>Email : dattadandale9850@gmail.com</h4>
+        <h4>Address : Shree Raj Rajeshwar Garage,
+Shop No - 1, Balapur Road , Old City, Akola, 444002 Maharashtra.
+
+</h4>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Contact;
